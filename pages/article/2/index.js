37caff97775aedbbd67d5dd63959dbb4562ba6bd
chuan <150107jasmine@gmail.com>
2024-02-18 23:05:50 +0800

    fix article 2 empty

---
+++ pages/article/2/index.js
@@ -0,0 +1,15 @@
+import Header from "../header";
+import Footer from "../footer";
+import React from "react";
+
+function Article() {
+  return (
+    <div className="flex flex-col">
+      <Header />
+      empty
+      <Footer />
+    </div>
+  );
+}
+
+export default Article;
